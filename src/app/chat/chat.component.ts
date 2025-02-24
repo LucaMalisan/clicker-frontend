e7d8800693305e161ed1278b5f72af476dd2a556
luca.malisan <luca.malisan@informaticon.com>
2025-02-24 18:55:27 +0100

    project init

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+import {CoreService} from "../core.service";
+
+interface ChatMessage {
+    username: string;
+    message: string;
+}
+
+@Component({
+    selector: 'app-chat',
+    standalone: true,
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.css'],
+})
+
+export class ChatComponent {
+
+    messages: ChatMessage[] = [];
+    messageInput: string = '';
+    isRegistered: boolean = false;
+
+    protected coreService: CoreService;
+
+    constructor() {
+    }
+
+    sendMessage() {
+        /*   if (this.messageInput.trim() && this.isRegistered) {
+               const chatMessage = {
+                   event: 'chat-message',
+                   username: 'Player1',
+                   message: this.messageInput
+               };
+           } */
+    }
+}
