47b6d7ce1e4fde0f653a258b23d4f99ea0ef4d5a
luca.malisan <luca.malisan@informaticon.com>
2025-03-24 10:41:03 +0100

    frontend logic for effects

---
+++ src/app/game-points/game-points.module.ts
@@ -0,0 +1,27 @@
+import {Component, NgModule, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CoreService} from "../core.service";
+
+@Component({
+    selector: 'points',
+    imports: [
+        CommonModule
+    ],
+    templateUrl: './gamePoints.component.html',
+    styleUrls: ['./gamePoints.component.css'],
+})
+export class GamePointsModule implements OnInit {
+
+    public points: number = 0;
+
+    constructor(protected coreService: CoreService) {
+    }
+
+    ngOnInit(): void {
+        this.coreService.initialized.subscribe(() => {
+            this.coreService.listen("points", (points: string) => {
+                this.points = parseInt(points)
+            });
+        })
+    }
+}
