3b6fbc4a3ed3ba82f7f925b78722a3c3b225f70a
luca.malisan <luca.malisan@informaticon.com>
2025-05-15 17:33:36 +0200

    send session key to backend

--- src/app/gameLoading/gameLoading.component.ts
+++ src/app/gameLoading/gameLoading.component.ts
@@ -31,7 +31,7 @@ export class GameLoadingComponent implements OnInit {
 
     ngOnInit() {
         this.coreService.initialized.subscribe(() => {
-            this.coreService.sendData('get-session-info', '', (response: string) => {
+            this.coreService.sendData('get-session-info', localStorage.getItem("session-key"), (response: string) => {
                 let json: ISessionInfo = JSON.parse(response);
 
                 if (!json.sessionKey) {
@@ -47,9 +47,8 @@ export class GameLoadingComponent implements OnInit {
 
             this.coreService.listen("start-game", () => this.router.navigate(["game"]));
 
-            this.coreService.listen('player-joined', (player: string) => {
-                console.log(player);
-                this.joinedPlayers.push(player)
+            this.coreService.listen('player-joined', (players: string) => {
+                this.joinedPlayers = JSON.parse(players);
             });
         });
     }
